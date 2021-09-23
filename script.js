e65a72c0f86221b2707d3fe9a8173ca71258c517
Nasrin Akter <nasrinaktermim2021@gmail.com>
2021-09-23 16:35:58 +0600

    second

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
+
 function compute()
 {
     var principal = document.getElementById("principal").value;
    var rate = document.getElementById("rate").value;
-   var years = document.getElementById("years").value; 
+   var years = document.getElementById("years").value;
+   var x = document.createElement("MARK"); 
    //principal * years * rate / 100;
    
-  /* function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-     var p=document.getElementById("rate_val").innerText=rateval; 
-     return p;  
-}*/
-function check()
-{
-    //Check if username field is empty
-    if(principal.value == ""){
-        alert("Please enter the Amount");
-        username.focus();
-        return false;
-  }
- //Check if email field is empty
-  if(principal.value == 0){
-        alert("Please enter the valid");
-        emailid.focus();
-        return false;
-  }
- //If all is well return true.
- alert("Form validation is successful.")
-  return true;
+  
+
+  var amount = principal * years * rate /100;
+  var year = new Date().getFullYear()+parseInt(years);
+
+function marks(y) {
+  var x = document.getElementById("highlight");
+    //x=y;
+    x.innerHTML=y;
+   return x.innerHTML;
+  
 }
-var interest = principal * years * rate /100;
-   var year = new Date().getFullYear()+parseInt(years);
+   //Check if Amount field is empty
+   if(principal== ""){
+    alert("Please enter the Amount");
+    document.getElementById("result").innerHTML.focus();
+    return false;
+}
+//Check if Amount value 0
+else if (principal<=0){
+    alert("Please enter the valid number, Amount must be positive and nonzero");
+    document.getElementById("result").innerHTML.focus();
+    return false;
+}
+//If all is well return true.
+else
+{alert("validation is successful.")
+//return true;
 
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
-
+document.getElementById("result").innerHTML="If you deposit "+marks(principal)+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+}
     
 }
         
